6a44f1dfe174ce5393cb7c8b3e3d90a213c05fa0
Kenny200 <58740689+Kenny200@users.noreply.github.com>
2026-04-11 16:09:02 -0400

    Enhance body count script with gender handling
    Added gender input and responses to the body count script.

--- body_count_script.py
+++ body_count_script.py
@@ -1,5 +1,6 @@
 #program to check user age and their bodycount
 import random
+import sys
 from datetime import datetime
 
 print(ascii_banner)
@@ -9,25 +10,29 @@ user_bdy_count = 0
 isHoe = False
 isAdult = False
 cur_date = datetime.now().date().strftime("%d/%m/%Y")
-print(cur_date)
-
-
 birth_date = input("\nEnter your dob:")
-print(birth_date)
-
 chck_birth_date = birth_date.split("/")
 chck_cur_date = cur_date.split("/")
 chck_gap = int(chck_cur_date[2]) - int(chck_birth_date[2])
-print(chck_birth_date)
 
-print(f'age = {chck_gap}')
 user_age = chck_gap
 if user_age >= 18:
     isAdult = True
 else:
     print("User is underage!")
 
+user_gender = input("Enter your gender")
+match user_gender:
+    case "male":
+        print("Hello king!")
+    case "female":
+        print("Hello queen!")
+    case "nonbinary":
+        sys.exit("404: gender not found") 
+
 user_bdy_count = input("\nWhat is your bodycount?")
 if user_bdy_count >= 5 and isAdult == False:
     print("Wowza, this early?")
     isHoe = True
+if user_bdy_count >= 5 and isAdult == True:
+    print("Wowza, at least you're a adult")
